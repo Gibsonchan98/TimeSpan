a096790d77ad20a99b27cad6e8f3f6e8e51e21ba
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2020-01-19 23:47:27 -0800

    Update timespan.h

--- timespan.h
+++ timespan.h
@@ -9,39 +9,43 @@
 using namespace std;
 
 class TimeSpan {
-  friend ostream &operator<<(ostream& output, const TimeSpan& timespan);
+    friend ostream &operator<<(ostream& output, const TimeSpan& timespan);
 
 public:
-  //explicit TimeSpan(int hours = 0, int minutes = 0, int seconds = 0);
-  explicit TimeSpan(float hr = 0, float min = 0, float sec = 0);
-  //sets time
-  bool setTime(const float& h, float& m, float& s);
-  // add
-  TimeSpan operator+(const TimeSpan& timespan) const;
-  //add number given
-  TimeSpan operator+=(const TimeSpan& timespan) const;
-  // subtract
-  TimeSpan operator-(const TimeSpan& timespan) const;
-  //subtract number given
-  TimeSpan operator-=(const TimeSpan& timespan) const;
-  // check equality
-  bool operator==(const TimeSpan& timespan) const;
-  // check if not equal
-  bool operator!=(const TimeSpan& timespan) const;
-  // check if equal or less than
-  bool operator<=(const TimeSpan& timespan) const;
-  // check if less than or equal to
-  bool operator>=(const TimeSpan& timespan) const;
-  // multiply timespan by a number
-  TimeSpan operator*(int number);
+    //constructor
+    explicit TimeSpan(float hr = 0, float min = 0, float sec = 0);
+    //destructor
+    virtual ~TimeSpan();
+    //sets time if values are negative
+    void setTime(const float& h, float& m, float& s);
+    // add
+    TimeSpan operator+(const TimeSpan& timespan) const;
+    //add number given
+    TimeSpan operator+=(const TimeSpan& timespan) const;
+    // subtract
+    TimeSpan operator-(const TimeSpan& timespan) const;
+    //subtract number given
+    TimeSpan operator-=(const TimeSpan& timespan) const;
+    // check equality
+    bool operator==(const TimeSpan& timespan) const;
+    // check if not equal
+    bool operator!=(const TimeSpan& timespan) const;
+    // check if equal or less than
+    bool operator<=(const TimeSpan& timespan) const;
+    // check if less than or equal to
+    bool operator>=(const TimeSpan& timespan) const;
+    // check if less than
+    bool operator<(const TimeSpan& timespan) const;
+    // check if greater than
+    bool operator>(const TimeSpan& timespan) const;
+    // multiply timespan by a number
+    TimeSpan operator*(int number);
 
 private:
     //declare private ints for class
     int hours;
     int minutes;
     int seconds;
-
-
 };
 
 #endif
